c431c6dca7c6a9b8aad0d261802c14c6f6c72aaa
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-07-09 16:53:44 -0700

    Add profile name finder
    This function finds nucamino alignment profile names for HCV genotypes.

---
+++ shrl/transform/align.py
@@ -0,0 +1,18 @@
+import typing as ty
+
+
+def profile_name(gt: str, subgt: ty.Optional[str]) -> str:
+    GENOTYPES = {"1", "2", "3", "4", "5", "6"}
+    if gt not in GENOTYPES:
+        raise ValueError(f"Invalid Genotype: {gt}")
+    if subgt is not None:
+        if type(subgt) is not str or len(subgt) > 1:
+            msg = f"Invalid subgenotype: {subgt}"
+            raise ValueError(msg)
+    if gt == "1":
+        if subgt == "b":
+            return "hcv1b"
+        else:
+            return "hcv1a"
+    else:
+        return f"hcv{gt}"
