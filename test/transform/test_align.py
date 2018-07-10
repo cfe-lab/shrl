b3a8a9b7426b8f57049b59c84455761e2137caee
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-07-09 17:08:25 -0700

    Add tests for fasta format checker

--- test/transform/test_align.py
+++ test/transform/test_align.py
@@ -1,4 +1,8 @@
+import re
 import unittest
+
+import hypothesis
+import hypothesis.strategies as st
 
 import shrl.transform.align as align
 
@@ -26,3 +30,26 @@ class TestProfileName(unittest.TestCase):
         ]
         for case in cases:
             self.assertRaises(ValueError, align.profile_name, *case)
+
+
+class TestEnsureFastaFormat(unittest.TestCase):
+    @hypothesis.given(st.text())
+    def test_fasta_formatted_leaves_formatted(self, content):
+        tmpl = "> Sequence-Ish\n{seq}"
+        seq = tmpl.format(seq=content)
+        self.assertEqual(seq, align.ensure_fasta_formatted(seq))
+
+    @hypothesis.given(st.text())
+    def test_fasta_formatter_is_idempotent(self, seq):
+        self.assertEqual(
+            align.ensure_fasta_formatted(seq),
+            align.ensure_fasta_formatted(align.ensure_fasta_formatted(seq)),
+        )
+
+    HEADER_PATTERN = re.compile("> [a-zA-Z0-9_ ]+")
+
+    @hypothesis.given(st.text())
+    def test_fasta_formatter_output_looks_like_a_fasta_file(self, seq):
+        outp = align.ensure_fasta_formatted(seq)
+        assert outp.startswith("> ")
+        assert self.HEADER_PATTERN.match(outp)
