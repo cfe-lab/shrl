0b7227fff531ee2f02ef86448a4fdd4c79705768
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-07-10 10:41:25 -0700

    Add namedtuples for entities from schema data

---
+++ shrl/transform/entities.py
@@ -0,0 +1,11 @@
+from shared_schema.data import schema_data
+
+Alignment = schema_data.namedtuple_for("Alignment")
+BehaviorData = schema_data.namedtuple_for("BehaviorData")
+ClinicalIsolate = schema_data.namedtuple_for("ClinicalIsolate")
+Isolate = schema_data.namedtuple_for("Isolate")
+LossToFollowUp = schema_data.namedtuple_for("LossToFollowUp")
+Person = schema_data.namedtuple_for("Person")
+Sequence = schema_data.namedtuple_for("Sequence")
+Substitution = schema_data.namedtuple_for("Substitution")
+TreatmentData = schema_data.namedtuple_for("TreatmentData")
