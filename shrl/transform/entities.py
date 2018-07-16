281f2480fe5d03b47df610cc278ad0d35df3c93c
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-07-16 15:27:33 -0700

    Add clinical data entity to target entities module

--- shrl/transform/entities.py
+++ shrl/transform/entities.py
@@ -2,6 +2,7 @@ from shared_schema.data import schema_data
 
 Alignment = schema_data.namedtuple_for("Alignment")
 BehaviorData = schema_data.namedtuple_for("BehaviorData")
+ClinicalData = schema_data.namedtuple_for("ClinicalData")
 ClinicalIsolate = schema_data.namedtuple_for("ClinicalIsolate")
 Isolate = schema_data.namedtuple_for("Isolate")
 LossToFollowUp = schema_data.namedtuple_for("LossToFollowUp")
