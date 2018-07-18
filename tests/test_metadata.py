03cfca33eed369f1ed2a36754065e4e21f3d3418
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-07-18 12:05:42 -0700

    Add some tests for metadata (from squasch)
    Because schema changes are involved, I'm starting with just the
    collaborator parser.

---
+++ tests/test_metadata.py
@@ -0,0 +1,60 @@
+import textwrap
+import unittest
+import uuid
+
+from shrl import metadata
+
+
+def parse_config_str(src):
+    parser = metadata.get_parser()
+    parser.read_string(src)
+    return parser
+
+
+class TestParseCollaborator(unittest.TestCase):
+    def test_load_collaborator(self):
+        src = textwrap.dedent(
+            """\
+            [collaborator]
+            id=af8e1177-a043-4d10-963d-5eb76af13566
+            name=asdf
+            """
+        )
+        parsed = parse_config_str(src)
+        extracted = metadata.collaborators(parsed)
+        expected_collab = metadata.Collaborator(
+            id=uuid.UUID("af8e1177-a043-4d10-963d-5eb76af13566"), name="asdf"
+        )
+        self.assertEqual(extracted, [expected_collab])
+
+    def test_load_multiple_collaborators(self):
+        src = textwrap.dedent(
+            """\
+            [collaborator.1]
+            id=af8e1177-a043-4d10-963d-5eb76af13566
+            name=asdf
+
+            [collaborator.2]
+            id=af8e1177-a043-4d10-963d-5eb76af13567
+            name = jkl
+            """
+        )
+        parsed = parse_config_str(src)
+        extracted = metadata.collaborators(parsed)
+        expected = [
+            metadata.Collaborator(
+                id=uuid.UUID("af8e1177-a043-4d10-963d-5eb76af13566"),
+                name="asdf",
+            ),
+            metadata.Collaborator(
+                id=uuid.UUID("af8e1177-a043-4d10-963d-5eb76af13567"),
+                name="jkl",
+            ),
+        ]
+        self.assertEqual(extracted, expected)
+
+    def test_missing_collaborator_raises_error(self):
+        src = ""
+        parsed = parse_config_str(src)
+        with self.assertRaises(metadata.MetadataError):
+            metadata.extract(parsed)
